465e17d89d90f037001ba9262c62653aba7f33b8
Newyork167 <newyork167@gmail.com>
2020-10-21 18:25:49 -0400

    Added logging and more complex algorithm with parameters

--- main.py
+++ main.py
@@ -10,28 +10,45 @@ from qiskit import (
 from qiskit.tools.monitor import job_monitor
 from qiskit.visualization import plot_histogram
 from timeit import default_timer as timer
+from itertools import permutations
+import logging
+from logging.config import fileConfig
 
-run_on_live_hardware = True
+# Setup logging
+fileConfig('Configuration/logging_config.ini')
+logger = logging.getLogger()
+
+# Setup config parser and determine whether to run on live hardware
+config = Configuration.NY167Config()
+run_on_live_hardware = config.get_boolean('app', 'run_on_live_hardware')
+
+# Set number of qubits and classical bits
+num_qbits = 4
+num_cbits = 4
+num_shots = 5000
 
 
 def build_test_circuit() -> QuantumCircuit:
     # Create a Quantum Circuit acting on the q register
-    circuit = QuantumCircuit(2, 2)
+    circuit = QuantumCircuit(num_qbits, num_cbits)
 
     # Add a H gate on qubit 0
-    circuit.h(0)
+    circuit.h([0, 2])
 
     # Add a CX (CNOT) gate on control qubit 0 and target qubit 1
-    circuit.cx(0, 1)
+    circuit.cx([0, 2], [1, 3])
+
+    circuit.mcx([0, 1, 2], 3)
 
     # Map the quantum measurement to the classical bits
-    circuit.measure([0, 1], [0, 1])
+    circuit.measure([0, 1, 2, 3], [0, 1, 2, 3])
 
     return circuit
 
 
 def run_test():
     circuit = build_test_circuit()
+    print(circuit.draw())
 
     start_time = timer()
 
@@ -44,7 +61,7 @@ def run_test():
         qcomp = provider.get_backend('ibmq_16_melbourne')
 
         # Execute the circuit on the live hardware
-        job = execute(experiments=circuit, backend=qcomp, shots=1000)
+        job = execute(experiments=circuit, backend=qcomp, shots=num_shots)
 
         job_monitor(job=job)
     else:
@@ -52,7 +69,7 @@ def run_test():
         simulator = Aer.get_backend('qasm_simulator')
 
         # Execute the circuit on the qasm simulator
-        job = execute(experiments=circuit, backend=simulator, shots=1000)
+        job = execute(experiments=circuit, backend=simulator, shots=num_shots)
 
     # Grab results from the job
     result = job.result()
@@ -60,20 +77,23 @@ def run_test():
     # Calculate total compute time taken
     end_time = timer()
 
-    print(f"Total compute/queue time: {end_time - start_time:.2f}s")
+    logger.debug(f"Total compute/queue time: {end_time - start_time:.2f}s")
 
     # Returns counts
     counts = result.get_counts(circuit)
-    # print("\nTotal count for 00 and 11 are:", counts)
 
-    # Draw the circuit
-    circuit.draw()
+    # Get algorithm time taken
+    try:
+        print(result)
+        for result in result.results:
+            logger.debug(f"Algorithm time taken: {result.time_taken}s")
+    except Exception as ex:
+        logger.debug(ex)
 
     return counts
 
 
 if __name__ == '__main__':
-    config = Configuration.NY167Config()
     IBMQ.save_account(config.get('api', 'api-token'))
 
     num_tests = 1
@@ -83,15 +103,14 @@ if __name__ == '__main__':
 
     print(df.head())
 
-    average_00 = df['00'].sum() / num_tests
-    average_11 = df['11'].sum() / num_tests
+    qbit_permutations = set([''.join(p[:num_qbits]) for p in permutations(['0', '1']*num_qbits)])
 
-    print(f"Average 00: {average_00}")
-    print(f"Average 11: {average_11}")
+    expected_values = ['0000', '0011', '0111', '1100']
+    total_expected = 0
 
-    if run_on_live_hardware:
-        average_01 = df['01'].sum() / num_tests
-        average_10 = df['10'].sum() / num_tests
-
-        print(f"Average 01: {average_01}")
-        print(f"Average 10: {average_10}")
+    for qbit_p in sorted(qbit_permutations):
+        average_qbit_p = df.get(qbit_p, np.zeros(1)).sum() / num_tests
+        if qbit_p in expected_values:
+            total_expected += average_qbit_p
+        logger.debug(f"Average {qbit_p}: {average_qbit_p}")
+    logger.debug(f"Error rate:   {1 - (total_expected / num_shots)}")
